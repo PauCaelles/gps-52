ad8cf9019282b080a0dd448780fd91c563f69896
PauCaelles <pau.caelles@gmail.com>
2022-12-13 17:38:15 +0100

    test finaaaal

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     string = input("Introdueix un string:")
 
     print("Quina transformació vols?")
-    print("[4] Text amb tot majuscules")
+    print("[1] Text amb tot majuscules")
     print("[2] Text amb tot minúscuies")
     print("[3] Text capitalitzat ")
 
     opcio = input("opció escollida: ")
 
-    if opcio == "4":
+    if opcio == "1":
         print(transform.to_upper_case(string))
     elif opcio == "2":
         print(transform.to_lower_case(string))
